038b8c5d66fcf0c6b62bf6931e49d7f05b530f0c
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-01-14 10:46:10 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+const employee = {
+  name: "Jasmine",
+  address: "54 Orchard St",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  const newEmployee = { ...obj };
+
+  newEmployee[key] = value;
+
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+
+  return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key, value) {
+  const revisedEmployee = { ...obj };
+
+  delete revisedEmployee[key];
+
+  return revisedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
+  delete obj[key];
+
+  return obj;
+}
